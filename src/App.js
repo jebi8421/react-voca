661e506ae45cab265d03e5ee4df8714f61497034
jebi8421 <jebi8421@naver.com>
2022-01-25 10:52:20 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import "./App.css";
-import Hello from "./component/Hello";
-import Welcome from "./component/Welcome";
+import "./index.css";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <h2>props : propterties</h2>
-      <Hello age={15} />
-      <Hello age={20} />
-      <Hello age={30} />
+      <Header />
     </div>
   );
 }
